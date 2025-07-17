b06b56d0a7158f2870b3a4883e174252e5588f8e
Flavian <onyangofle@gmail.com>
2025-07-18 00:17:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#create our own function
+#def hello():
+    #print("hello")
+
+#name = input("What's your name?")
+#hello()
+#print(name)
+
+#def hello(to):
+    #print("hello,",to)
+#name = input("what's your name?")
+#hello(name)
+
+#def hello(to):
+    #print("hello,", to)
+#name = input("what's your name?")
+#hello(name)
+#hello()
+
+#def main():
+    #name = input("what's your name?")
+    #hello(name)
+    #hello()
+#def hello(to="world"):
+    #print("hello,", to)
+#main()
+
+
+#study drill
+def hello(to):
+    print("hello,",to)
+    my name = input("What's your name?")
+hello
+print(my name)
+
+
+
